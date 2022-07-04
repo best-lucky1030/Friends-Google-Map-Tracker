495c757dfd335f1d2ba84b70a8705e2e60607bc1
best-lucky1030 <bestlucky1030@gmail.com>
2022-07-05 02:33:27 +0300

    add friend-search and friend-card components

--- client/src/app/features/friends/friend.module.ts
+++ client/src/app/features/friends/friend.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CoreModule } from 'src/app/core/core.module';
 import { FriendMapComponent } from './components/friend-map/friend-map.component';
+import { FriendCardComponent } from './components/friend-card/friend-card.component';
+import { FriendSearchComponent } from './components/friend-search/friend-search.component';
 
 @NgModule({
   declarations: [
     FriendMapComponent,
+    FriendCardComponent,
+    FriendSearchComponent,
   ],
   imports: [CoreModule],
   exports: [
