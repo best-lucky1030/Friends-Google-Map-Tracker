a10df0e7f6ad987e4695bbba05d0976b8de2ba94
best-lucky1030 <bestlucky1030@gmail.com>
2022-07-04 23:51:16 +0300

    add friend-map component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FriendMapComponent } from './features/friends/components/friend-map/friend-map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    FriendMapComponent
   ],
   imports: [
     BrowserModule,
